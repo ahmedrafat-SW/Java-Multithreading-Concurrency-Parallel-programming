2b4f6fce1facfaa7f409cd4bc776143ad1632369
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2024-11-27 20:01:18 +0200

    Added Thread Daemon explicit InterruptedException handler

---
+++ src/main/java/com/dev/thread/DaemonThreadExplicitHandler.java
@@ -0,0 +1,23 @@
+package com.dev.thread;
+
+public class DaemonThreadExplicitHandler {
+
+    public static void main(String [] args) {
+        Thread thread = new Thread(new SleepingThread());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class SleepingThread implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(1000000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/dev/thread/TreadDaemon.java
+++ src/main/java/com/dev/thread/TreadDaemon.java
@@ -7,6 +7,8 @@ public class TreadDaemon {
     public static void main(String[] args) {
 
         Thread thread = new Thread(new LongComputationTask(new BigInteger("200000000000000"), new BigInteger("1000000000000")));
+        // Daemon or user Thread not exit if the program exits
+        thread.setDaemon(true);
         thread.start();
         thread.interrupt();
     }
@@ -30,7 +32,7 @@ public class TreadDaemon {
             BigInteger result = BigInteger.ONE;
 
             for (BigInteger i = BigInteger.ZERO; i.compareTo(power) !=0 ; i = i.add(BigInteger.ONE)){
-//                explicitly handle Thread interruption because this method doesn't handle ThreadInterruptException
+//                explicitly handle Thread interruption because this method doesn't handle InterruptedException
                 if (Thread.currentThread().isInterrupted()){
                     System.out.println("Prematurely interrupted Computation");
                     return BigInteger.ZERO;

05e614fe6b59abcfd8ef115381f000926f1f36dd
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2024-11-27 19:24:18 +0200

    Added Thread Daemon and Thread interruption

---
+++ src/main/java/com/dev/thread/TreadDaemon.java
@@ -0,0 +1,44 @@
+package com.dev.thread;
+
+import java.math.BigInteger;
+
+public class TreadDaemon {
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("200000000000000"), new BigInteger("1000000000000")));
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class LongComputationTask  implements Runnable{
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power){
+            this.base = base;
+            this.power = power;
+        }
+
+
+        @Override
+        public void run() {
+            System.out.println( base + " ^ "+ power + " = " + calculatePow(base, power));
+        }
+
+        private BigInteger calculatePow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) !=0 ; i = i.add(BigInteger.ONE)){
+//                explicitly handle Thread interruption because this method doesn't handle ThreadInterruptException
+                if (Thread.currentThread().isInterrupted()){
+                    System.out.println("Prematurely interrupted Computation");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+
+            return result;
+        }
+    }
+}
